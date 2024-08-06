334a9e0b3677035c12de815e5c5d75c8e29caad2
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-08-06 20:58:05 +0100

    initial commit - File structure

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Stopwatch from "./component/Stopwatch";
+
+export default function App() {
+  return <Stopwatch />;
+}
---
+++ src/component/Stopwatch.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Stopwatch() {
+  return <div>Stopwatch</div>;
+}
