9472e63f02a9a3401ba098ef332c95ffd606761f
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-08-06 21:05:16 +0100

    created app logic

--- src/component/Stopwatch.jsx
+++ src/component/Stopwatch.jsx
@@ -1,6 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Stopwatch() {
+  const [time, setTime] = useState({
+    ms: 0,
+    s: 0,
+    m: 0,
+    h: 0,
+  });
+  const [timerInterval, setTimerInterval] = useState();
+  const handleStart = () => {
+    run();
+    setTimerInterval(setInterval(run, 10));
+  };
+
+  let updatedMs = time.ms,
+    updatedS = time.s,
+    updatedM = time.m,
+    updatedH = time.h;
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH++;
+      updatedS = 0;
+    }
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+    if (updatedMs === 100) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({
+      ms: updatedMs,
+      s: updatedS,
+      m: updatedM,
+      h: updatedH,
+    });
+  };
+
+  const handleReset = () => {
+    clearInterval(timerInterval);
+    setTime({
+      ms: 0,
+      s: 0,
+      m: 0,
+      h: 0,
+    });
+  };
+  const handleStop = () => {
+    clearInterval(timerInterval);
+  };
   return (
     <>
       <div className="flex">
